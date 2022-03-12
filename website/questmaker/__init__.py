f02e3dbf7fbd223ef1ea0ca419d8dbc90f608b8b
Ilya Pavlov <pavlov001.is@gmail.com>
2022-03-12 17:05:09 +0300

    Create db script #32

--- website/questmaker/__init__.py
+++ website/questmaker/__init__.py
@@ -7,8 +7,6 @@ functions:
     * create_app - create and configure the questmaker app that can be run from python code
 """
 
-import os
-
 from flask import Flask, render_template
 
 
@@ -18,25 +16,12 @@ def create_app(test_config=None):
     :return: configured app
     """
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        # DATABASE=path_to_db
-    )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    app.config.from_pyfile('config.py', silent=True)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    from . import db
+    db.init_app(app)
 
-    # main page
     @app.route('/')
     def index():
         return render_template('index.html')

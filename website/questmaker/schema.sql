f38d8e0e3e0a0d6e13f9ba2eace058b9fae727ae
Ilya Pavlov <pavlov001.is@gmail.com>
2022-03-20 18:09:19 +0300

    Fix schema.sql script #46

--- website/questmaker/schema.sql
+++ website/questmaker/schema.sql
@@ -57,7 +57,7 @@ DROP TABLE IF EXISTS files CASCADE;
 CREATE TABLE files (
     f_id SERIAL PRIMARY KEY, 
     url_for_file TEXT NOT NULL,
-    file_type_id INTEGER NOT NULL REFERENCES file_types (file_type_id) ON DELETE RESTRICT
+    type_id INTEGER NOT NULL REFERENCES file_types (f_type_id) ON DELETE RESTRICT
 );
 
 DROP TABLE IF EXISTS quest_files CASCADE;

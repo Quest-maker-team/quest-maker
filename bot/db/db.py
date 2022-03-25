74be1a143ad5b9f25d77b96233e885af6f7b9c51
gchevykalov <gcevykalov@gmail.com>
2022-03-25 16:37:01 +0300

    Add a simple interaction with the database #74

--- bot/db/db.py
+++ bot/db/db.py
@@ -38,13 +38,14 @@ class Database(metaclass=MetaSingleton):
             self.connection = psycopg2.connect(dbname=name, user=user, password=password, host=host, port=port)
         return self.connection
 
-    def close(self):
+    def __del__(self):
         """
         Close database connection if exists
         :param self: instance
         """
         if self.connection is not None:
             self.connection.close()
+            self.connection = None
 
 
 def select_one(query, params):
@@ -53,13 +54,12 @@ def select_one(query, params):
     :param query: selection request
     :param params: request parameters
     :return: matching row from table
+    :return: None if there are no matching rows
     """
     with Database().connect() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cursor:
             cursor.execute(query, params)
             res = cursor.fetchone()
-            if not res:
-                return False
 
     return res
 
@@ -70,101 +70,108 @@ def select_all(query, params):
     :param query: selection request
     :param params: request parameters
     :return: all matching rows from table
+    :return: None if there are no matching rows
     """
     with Database().connect() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cursor:
             cursor.execute(query, params)
             res = cursor.fetchall()
-            if not res:
-                return False
 
     return res
 
 
-def get_quest_by_id(quest_id):
+def get_quest_title(quest_id):
     """
-    Find quest in table quest by id
+    Find quest title in table quest by id
     :param quest_id: quest id
-    :return: matching row from table quest
+    :return: name of the quest with the specified id
+    :return: None if quest with the same id don't exist
     """
-    return select_one('SELECT * FROM quest WHERE id= %s', (quest_id, ))
+    try:
+        name = select_one("SELECT title FROM quests WHERE quest_id= %s", (quest_id, ))
+        if name:
+            return name[0]
+        else:
+            return None
+    except:
+        #In case a non-integer is entered
+        return None
 
 
-def get_files(object_id, type):
+def get_question_by_id(question_id):
     """
-    Finds all files related to the object
-    :param object_id: object id
-    :param type: type of object such as 'quest', 'place' etc.
-    :return: all matching rows from table files
-    """
-    object_type_id = select_one("SELECT id FROM object_type WHERE type= %s", (type, ))[0]
-    if not object_type_id:
-        return False
-
-    return select_all('SELECT url_for_file, type_of_file FROM files '
-                      'WHERE id= %s AND type_of_object_id= %s ORDER BY id', (object_id, object_type_id, ))
-
-
-def get_place_by_id(place_id):
-    """
-    Find place in table place by id
-    :param place_id: place id
-    :return: matching row from table place
-    """
-    return select_one('SELECT * FROM place WHERE id= %s', (place_id, ))
-
-
-def get_directions(place_id):
-    """
-    Find all directions from received place
-    :param place_id: place id
-    :return: all matching rows from table directions
-    """
-    return select_all('SELECT next_place_id, description FROM directions WHERE cur_place_id= %s', (place_id, ))
-
-
-def get_questions(place_id):
-    """
-    Find all questions related to the place
-    :param place_id: place id
-    :return: all matching rows from table question
-    """
-    return select_all('SELECT * FROM question WHERE place_id= %s ORDER BY id', (place_id, ))
-
-
-def get_answer(question_id):
-    """
-    Find answer related to the question
+    Find question in table questions by id
     :param question_id: question id
-    :return: all matching rows from table answer
+    :return: question id, question text and question type name tuple
+    :return: None if question with the same id don't exist
     """
-    return select_all('SELECT id, answer_text FROM answer WHERE question_id= %s', (question_id, ))
+    try:
+        return select_one('SELECT question_id, question_text, q_type_name FROM questions INNER JOIN question_types '
+                          'ON questions.q_type_id= question_types.q_type_id WHERE question_id= %s', (question_id, ))
+    except:
+        #In case a non-integer is entered
+        return None
 
 
-def get_possible_answers(question_id):
+def get_first_question(quest_id):
     """
-    Find all possible answers related to the question
+    Find first question related to the quest
+    :param quest_id: quest id
+    :return: first question id, question text and question type name tuple
+    :return: None in case of failure
+    """
+    try:
+        bogus_question_id = select_one('SELECT question_id FROM questions INNER JOIN question_types '
+                                       'ON questions.q_type_id= question_types.q_type_id WHERE quest_id= %s '
+                                       'AND q_type_name= %s', (quest_id, 'start', ))[0]
+        real_question_id = select_one('SELECT next_question_id FROM answer_options WHERE question_id= %s',
+                                      (bogus_question_id, ))[0]
+        return get_question_by_id(real_question_id)
+    except:
+        return None
+
+
+def get_answer_options(question_id):
+    """
+    Find answer options related to the question
     :param question_id: question id
-    :return: all matching rows from table possible_answer
+    :return: a list of tuples with values option_text, points, next_question_id
+    :return: None in case of failure
     """
-    return select_all('SELECT id, possible_ans_text FROM possible_answer WHERE question_id= %s', (question_id, ))
+    try:
+        return select_all('SELECT option_text, points, next_question_id FROM answer_options WHERE question_id= %s',
+                          (question_id, ))
+    except:
+        return None
+
+
+def get_movement(question_id):
+    """
+    Find movement related to the question
+    :param question_id: question id
+    :return: tuple with values next_question_id, coords, radius
+    :return: None in case of failure
+    """
+    try:
+        return select_one('SELECT next_question_id, coords, radius FROM movements INNER JOIN places '
+                          'ON movements.place_id= places.place_id WHERE question_id= %s', (question_id, ))
+    except:
+        return None
 
 
 def get_hints(question_id):
     """
     Find all hints related to the question
     :param question_id: question id
-    :return: all matching rows from table hints
+    :return: a list of tuples with values fine, hint_text
+    :return: None in case of failure
     """
-    return select_all('SELECT id, hint_text, fine FROM hints WHERE question_id= %s', (question_id, ))
+    try:
+        return select_all('SELECT fine, hint_text FROM hints WHERE question_id= %s', (question_id, ))
+    except:
+        return None
 
 
-def check_user_history(user_id, quest_id):
-    """
-    Checks if the given quest has been interrupted
-    :param user_id: user id
-    :param quest_id: quest id
-    :return: matching row from table history or false if quest was complited
-    """
-    return select_one('SELECT last_place_id, final_score FROM history WHERE user_id= %s AND '
-                      'quest_id= %s AND is_finished= false', (user_id, quest_id, ))
+if __name__ == '__main__':
+    info = get_first_question('0')
+    print(info)

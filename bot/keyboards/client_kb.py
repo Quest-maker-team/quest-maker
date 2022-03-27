bbb69af1a844b13e004b3abfd6ecc81502da16e5
aVorotnikov <AndrewVorotnikov@yandex.ru>
2022-03-27 19:59:07 +0300

    Add doc string #70

--- bot/keyboards/client_kb.py
+++ bot/keyboards/client_kb.py
@@ -7,6 +7,10 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
 def create_keyboard(answers):
+    """Function to create keyboard.
+    :param answers: list of strings for answers.
+    :return keyboard markup
+    """
     kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
     for answer in answers:
         kb_client.add(KeyboardButton(answer))

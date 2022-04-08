99bfcb0202c95d266e778b6d83008f3f96150e81
Ilya Pavlov <pavlov001.is@gmail.com>
2022-04-09 02:02:31 +0300

    Fix bfs #146

--- website/questmaker/bfs.py
+++ website/questmaker/bfs.py
@@ -1,14 +1,15 @@
 from queue import SimpleQueue
+from .quest import Question
 
 
 class BFS:
-    def __init__(self, quest):
-        self.__quest__ = quest
+    def __init__(self, node):
+        self.__start_node__ = node
 
     def __iter__(self):
         self.__visited__ = set()
         self.__queue__ = SimpleQueue()
-        self.__queue__.put(self.__quest__.first_question)
+        self.__queue__.put(self.__start_node__)
         return self
 
     def __next__(self):
@@ -16,18 +17,17 @@ class BFS:
             raise StopIteration
 
         cur = self.__queue__.get()
-        cur_type = type(cur)
 
         self.__visited__.add(cur)
 
-        if cur_type == 'Question':
+        if isinstance(cur, Question):
             for ans in cur.answers:
                 if ans not in self.__visited__:
                     self.__queue__.put(ans)
             for move in cur.movements:
                 if move not in self.__visited__:
                     self.__queue__.put(move)
-        else:  # cur_type is answer or movement
+        else:  # current node type is Answer or Movement
             if cur.next_question not in self.__visited__:
                 self.__queue__.put(cur.next_question)
 

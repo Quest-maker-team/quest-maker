98af669705846e96409674eace37b7d2171e35c0
Aleksandr Shvarts <shvartcaleksandr@gmail.com>
2022-04-11 01:36:45 +0300

    Fix styles #145

--- website/assets/js/blockRedactor.js
+++ website/assets/js/blockRedactor.js
@@ -1,48 +1,3 @@
-export function init () {
-    console.log("In init map");
-    
-    myMap = new ymaps.Map("map", {
-        center: [57.5262, 38.3061], // Углич
-        zoom: 11
-    }, {
-        balloonMaxWidth: 200,
-        searchControlProvider: 'yandex#search'
-    });
-    console.log(myMap);
-    // Обработка события, возникающего при щелчке
-    // левой кнопкой мыши в любой точке карты.
-    // При возникновении такого события откроем балун.
-    myMap.events.add('click', function (e) {
-        if (!myMap.balloon.isOpen()) {
-            let coords = e.get('coords');
-            myMap.balloon.open(coords, {
-                contentHeader:'Событие!',
-                contentBody:'<p>Кто-то щелкнул по карте.</p>' +
-                    '<p>Координаты щелчка: ' + [
-                    coords[0].toPrecision(6),
-                    coords[1].toPrecision(6)
-                    ].join(', ') + '</p>',
-                contentFooter:'<sup>Щелкните еще раз</sup>'
-            });
-        }
-        else {
-            myMap.balloon.close();
-        }
-    });
-
-    // Обработка события, возникающего при щелчке
-    // правой кнопки мыши в любой точке карты.
-    // При возникновении такого события покажем всплывающую подсказку
-    // в точке щелчка.
-    myMap.events.add('contextmenu', function (e) {
-        myMap.hint.open(e.get('coords'), 'Кто-то щелкнул правой кнопкой');
-    });
-    
-    // Скрываем хинт при открытии балуна.
-    myMap.events.add('balloonopen', function (e) {
-        myMap.hint.close();
-    });
-}
 export class BlockRedactor {
     static addTextRedactor(form, label, text){
         form.innerHTML +=
@@ -74,10 +29,14 @@ export class BlockRedactor {
                     "<span class=\"input-group-text\"> Координаты </span>" +
                     "<input type=\"text\" class=\"form-control\" id=\"moveCoords\" value=" + question.movements[0].place.coords + ">" +
                 "</div>"+
+            "</div>"+
+            "<div class='col-8'>"+
                 "<div class='input-group'>" +
                     "<span class=\"input-group-text\"> Радиус(м) </span>" +
                     "<input type=\"text\" class=\"form-control\" id=\"moveRadius\"  value=" +question.movements[0].place.radius + ">" +
-            "<div class=\"col-3\">" +
+                  "</div>"+
+            "</div>"+ 
+            "<div class=\"col-8\">" +
                 "<div class='input-group'>" +
                     "<span class=\"input-group-text\"> Очки </span>" +
                     "<input type=\"text\" class=\"form-control\" id=\"movePoints\"  value=" + question.answer_options[0].points + ">" +
@@ -119,12 +78,8 @@ export class BlockRedactor {
     static createMovementRedactor(form, question){
 
         this.addTextRedactor(form, "Перемещение", question.text);
-        form.innerHTML+='<style>'+
-        'html, body, #map {'+
-        '    width: 100%; height:466px; padding: 0; margin: 0;'+
-        '}'+
-        '</style>'+
-        '<div id="map"></div>';
+        form.innerHTML+=
+        '<div class="z-depth-1-half map-container" style="height: 500px" id="map"></div>';
         let myMap;
         let mapId = document.getElementById("map");
         console.log(mapId);

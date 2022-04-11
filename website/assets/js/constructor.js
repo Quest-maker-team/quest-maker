cdf7e38c79bdb23df8b1a910c04feaf2f99a165e
Aleksandr Shvarts <shvartcaleksandr@gmail.com>
2022-04-11 23:50:44 +0300

    Fix merge bug #141

--- website/assets/js/constructor.js
+++ website/assets/js/constructor.js
@@ -25,13 +25,40 @@ let targetEndpoint = {
     connectionsDetachable: true,
     anchor: [ 0.5, 0, 0, -1 ],
 };
-
 Quest.loadQuest(2).then(newQuest =>{
-    let quest = newQuest;
+     quest = newQuest;
     Render.render(quest, instance, sourceEndpoint, targetEndpoint);
     /*document.getElementById("save").onclick = () => {
         quest.save().then(() => console.log("save"));
     }*/
+    document.getElementById("addMBtn").onclick = () => {
+        let movements = quest.data.questions.filter(item => item.type == "movement");
+        console.log(movements);
+        let maxMovement = movements.reduce((acc, curr) =>
+        acc.movements[0].movement_id >= curr.movements[0].movement_id  ? acc : curr);
+        console.log(maxMovement == undefined);
+        let maxId = (maxMovement != undefined ? maxMovement.movements[0].movement_id+1 : 1);
+        console.log(maxId);
+        let questionId = createNewBlock("movement", "Новое перемещение", Render.renderMovement);
+        console.log(quest.data.questions);
+        quest.data.questions.slice(-1)[0].movements.push({
+            "movement_id": maxId,
+            "next_question_id": undefined,
+            "place": {
+                "coords": "(0.0,0.0)",
+                "place_id": undefined,
+                "radius": 0,
+                "time_close": "Sun, 12 Aug 2001 19:00:00 GMT",
+                "time_open": "Sun, 12 Aug 2001 09:00:00 GMT"
+            }
+        });
+        Quest.pushMovement(quest, questionId, maxId);
+    }
+    
+    document.getElementById("addQBtn").onclick = () => {
+        let questionId =  createNewBlock("open", "Новый открытый вопрос", Render.renderOpenQuestion);
+        Quest.pushQuestion(quest, questionId);
+    }
     return quest;
 }).then(quest => {
     instance.bind(EVENT_CONNECTION, (connection) => {
@@ -89,31 +116,4 @@ export function createNewBlock(type, text, renderFunction){
    renderFunction(quest, quest.data.questions.slice(-1)[0], instance, sourceEndpoint, targetEndpoint, "absolute");
    return newBlockId;
 }
- document.getElementById("addMBtn").onclick = () => {
-    let movements = quest.data.questions.filter(item => item.type == "movement");
-    console.log(movements);
-    let maxMovement = movements.reduce((acc, curr) =>
-    acc.movements[0].movement_id >= curr.movements[0].movement_id  ? acc : curr);
-    console.log(maxMovement == undefined);
-    let maxId = (maxMovement != undefined ? maxMovement.movements[0].movement_id+1 : 1);
-    console.log(maxId);
-    let questionId = createNewBlock("movement", "Новое перемещение", Render.renderMovement);
-    console.log(quest.data.questions);
-    quest.data.questions.slice(-1)[0].movements.push({
-        "movement_id": maxId,
-        "next_question_id": undefined,
-        "place": {
-            "coords": "(0.0,0.0)",
-            "place_id": undefined,
-            "radius": 0,
-            "time_close": "Sun, 12 Aug 2001 19:00:00 GMT",
-            "time_open": "Sun, 12 Aug 2001 09:00:00 GMT"
-        }
-    });
-    Quest.pushMovement(quest, questionId, maxId);
-}
-
-document.getElementById("addQBtn").onclick = () => {
-    let questionId =  createNewBlock("open", "Новый открытый вопрос", Render.renderOpenQuestion);
-    Quest.pushQuestion(quest, questionId);
-}
+ 

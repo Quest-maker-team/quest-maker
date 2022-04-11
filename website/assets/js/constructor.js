f7b924b08a7ad8de02d06ff4c4e17f1be2772605
Daniil Baev <baev.daniil.2002@gmail.com>
2022-04-11 21:42:36 +0300

    Fix randor movement connect #141

--- website/assets/js/constructor.js
+++ website/assets/js/constructor.js
@@ -30,9 +30,9 @@ let targetEndpoint = {
 Quest.loadQuest(2).then(newQuest =>{
     let quest = newQuest;
     Render.render(quest, instance, sourceEndpoint, targetEndpoint);
-    document.getElementById("save").onclick = () => {
+    /*document.getElementById("save").onclick = () => {
         quest.save().then(() => console.log("save"));
-    }
+    }*/
     return quest;
 });
 

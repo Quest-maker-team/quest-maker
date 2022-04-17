935b793c5de305a51fd7ac14bdd56d92d827e77a
gchevykalov <gcevykalov@gmail.com>
2022-04-17 12:29:36 +0300

    Change project settings #182

--- website/questmaker/static/script/index.js
+++
@@ -1,8 +0,0 @@
-$(document).on("scroll", window, function () {
-    if ($(window).scrollTop()>200){
-        $(".scroll").show();
-    }
-    else{
-        $(".scroll").hide();
-    }
-});
--- website/webpack.config.js
+++ website/webpack.config.js
@@ -2,8 +2,11 @@ const path = require('path');
 const webpack = require('webpack');
 
 const TerserPlugin = require('terser-webpack-plugin');
-const {CleanWebpackPlugin}  = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin }  = require('clean-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+
 // Get local IP Address
 let os = require('os');
 let interfaces = os.networkInterfaces();
@@ -21,16 +24,9 @@ ip_address = addresses[0]
 
 // take debug mode from the environment
 const debug = (process.env.NODE_ENV !== 'production');
-const hashType = debug ? '[hash]': '[contentHash]'
-const rootAssetPath = path.join(__dirname, 'assets');
+const hashType = debug ? '[hash]': '[contentHash]';
+console.log(debug);
 let publicHost
-
-if (ip_address !== undefined) {
-    publicHost = debug ? 'http://' + ip_address + ':2992' : '';
-}
-else {
-    publicHost = debug ? 'http://0.0.0.0:2992' : '';
-}
 
 if (ip_address !== undefined) {
     publicHost = debug ? 'http://' + ip_address + ':2992' : '';
@@ -41,37 +37,59 @@ else {
 
 module.exports = {
   // configuration
+  // watch: true,
+  mode: debug ? 'development' : 'production',
   context: __dirname,
   entry: {
     main_js: path.join(__dirname, 'assets', 'js', 'constructor.js'),
-    /*main_css: [
-      path.join(__dirname, 'assets', 'css', 'main.css'),
-    ],*/
   },
   output: {
     path: path.resolve(__dirname, 'questmaker', 'static'),
     publicPath: `${publicHost}/static/`,
-    filename: "script/[name]." + hashType + ".js",
-    chunkFilename: "script/[name]." + hashType + ".chunk.js"
+    filename: "js/[name]." + hashType + ".js",
+    chunkFilename: "js/[name]." + hashType + ".chunk.js"
   },
   optimization: {
-    minimize: true,
+    // minimize: true,
     minimizer: [
+      new CssMinimizerPlugin(),
       new TerserPlugin({
         test: /\.js(\?.*)?$/i,
       }),
     ],
   },
+  module: {
+    rules: [
+        {
+          test: /\.js$/,
+          //exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.(?:ico|gif|png|jpg|jpeg|svg|woff(2)?|eot|ttf|otf)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[folder]/[name].' + hashType + '.[ext]',
+              },
+            },
+          ],
+        },
+        {
+            test: /\.(scss|css)$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        },
+    ],
+  },
   plugins: [
     new CleanWebpackPlugin({
       cleanOnceBeforeBuildPatterns: [
-        'script/*',
-        '!script/bootstrup/**',
-        '!script/jquery/**',
-        '!script/index.js*',
+        '*',
+        '!manifest.json',
     ],
     }),
-    new WebpackManifestPlugin({publicPath:"/static/"}),
+    new MiniCssExtractPlugin({ filename: 'css/[name].' + hashType + '.css', }),
+    new WebpackManifestPlugin({ publicPath:"/static/" }),
   ],
-  
 }

787a721868f679b180c4829ce9f13868eeb36462
Daniil Baev <baev.daniil.2002@gmail.com>
2022-04-25 12:39:20 +0300

    Add request for quest update #184

--- website/assets/js/quest.js
+++ website/assets/js/quest.js
@@ -81,14 +81,14 @@ export class Quest {
         return Quest.makeRequest('POST', url, answer);
     }
 
+    static updateQuest(id, questParams){
+        const url = 'api/quest' + id;
+        return Quest.makeRequest('PUT', url, questParams);
+    }
+
     static updateAnswer(id, answer) {
         const url = 'api/answer_option/' + id;
         return Quest.makeRequest('PUT', url, answer);
-    }
-
-    static deleteEntity(type, id) {
-        const url = 'api/' + type + '/' + id;
-        return Quest.makeRequest('DELETE', url);
     }
 
     static updateQuestion(id, question) {
@@ -103,6 +103,11 @@ export class Quest {
 
     static disconnect(type1, type2, id1) {
         const url = 'api/' + type1 + '/' + id1 + '/' + type2;
+        return Quest.makeRequest('DELETE', url);
+    }
+
+    static deleteEntity(type, id) {
+        const url = 'api/' + type + '/' + id;
         return Quest.makeRequest('DELETE', url);
     }
 

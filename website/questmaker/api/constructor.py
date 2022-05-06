6a5bfaa198aba47845c574e395161a2d8a9a132e
Ilya Pavlov <pavlov001.is@gmail.com>
2022-05-07 01:09:45 +0300

    Remove extra import

--- website/questmaker/api/constructor.py
+++ website/questmaker/api/constructor.py
@@ -1,6 +1,6 @@
 from questmaker.quest import Quest, Question, Place, Hint, Answer, Movement, File, update_from_dict
 from questmaker.quest_container import EntityType, QuestContainer
-from questmaker.db import get_draft, update_draft, write_draft, remove_draft, get_draft_for_update, remove_quest
+from questmaker.db import get_draft, update_draft, write_draft, remove_draft, get_draft_for_update
 
 from flask_login import current_user, login_required
 from flask import Blueprint, jsonify, request, g, session

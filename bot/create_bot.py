93a9240d41eac5d00cdab55fa77d3b8affd14043
aVorotnikov <AndrewVorotnikov@yandex.ru>
2022-05-23 23:40:17 +0300

    Fix bot bugs #282

--- bot/create_bot.py
+++ bot/create_bot.py
@@ -14,20 +14,20 @@ from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
 storage=MemoryStorage()
 
+TG_BOT_TOKEN = os.environ['TG_BOT_TOKEN']
 WEBHOOK_HOST = os.environ['WEBHOOK_HOST']  # Domain name or IP addres which your bot is located.
-WEBHOOK_PORT = int(os.environ['WEBHOOK_PORT'])  # Telegram Bot API allows only for usage next ports: 443, 80, 88 or 8443
-WEBHOOK_URL_PATH = os.environ['WEBHOOK_URL_PATH']  # Part of URL
+WEBHOOK_PATH = f"/webhook/{TG_BOT_TOKEN}"  # Part of URL
 
-WEBHOOK_URL = f"https://{WEBHOOK_HOST}:{WEBHOOK_PORT}{WEBHOOK_URL_PATH}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # Web app settings:
 #   Use LAN address to listen webhooks
 #   Use any available port in range from 1024 to 49151 if you're using proxy, or WEBHOOK_PORT if you're using direct webhook handling
-WEBAPP_HOST = 'localhost'
-WEBAPP_PORT = 3001
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 loop = asyncio.get_event_loop()
-bot = Bot(token=os.environ['TG_BOT_TOKEN'], loop=loop)
+bot = Bot(token=TG_BOT_TOKEN, loop=loop)
 dp = Dispatcher(bot, storage=storage)
 logging.basicConfig(level=logging.INFO)
 dp.middleware.setup(LoggingMiddleware())
